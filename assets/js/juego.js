16ed70c175e843edaaa0d9679c461808e233ea34
unknown <igna.dom@gmail.com>
2023-07-31 20:19:24 -0300

    Todo preparado

---
+++ assets/js/juego.js
@@ -0,0 +1,22 @@
+/*
+ * 2C = Two of Clubs   (Tréboles)
+ * 2H = Two of Hearts  (Corazones)
+ * 2D = Two of Diamons (Diamantes)
+ * 2S = Two of Sword   (Espadas)
+*/
+
+let deck = [];
+const tipos = ['C','D','H','S'];
+
+const crearDeck = () =>{
+    for(let i = 2;i <= 10;i++){
+        for(let tipo of tipos){
+            
+        }
+        // deck.push(i + 'C');
+    }
+    console.log(deck);
+}
+
+
+crearDeck();
